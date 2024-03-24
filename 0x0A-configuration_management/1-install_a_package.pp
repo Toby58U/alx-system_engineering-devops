56a802744f791d8e3d4c47736f2afe34b4740ed7
Toby58U <tobennaumeh001@gmail.com>
2024-03-24 13:23:52 +0000

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 # Using Puppet, install flask from pip3 
 # Install an especific version Werkzeug (2.1.1)
 
-package { 'Werkzeug':
-  ensure => '2.1.1',
-  provider => 'pip3',
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+package { 'Werkzeug':
+  ensure => '2.1.1',
+  provider => 'pip3',
 }
