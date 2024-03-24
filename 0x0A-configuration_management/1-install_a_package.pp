6566c07dc8eb1dc7c2316ea5d80a9773de538909
Toby58U <tobennaumeh001@gmail.com>
2024-03-24 13:46:41 +0000

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,16 @@
 # Using Puppet, install flask from pip3 
 # Install an especific version Werkzeug (2.1.1)
 
-package { 'flask':
+package { ['flask', 'werkzeug']:
   ensure   => '2.1.0',
   provider => 'pip3'
 }
-package { 'Werkzeug':
-  ensure => '2.1.1',
-  provider => 'pip3',
+
+exec { 'install_werkzeug':
+  command     => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  environment => ['PATH=/usr/bin:/usr/local/bin'],
+  unless      => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+  require     => Package['python3-pip'],
 }
+
